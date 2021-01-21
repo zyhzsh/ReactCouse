4d991eb610b07f6c5bf52f7b94fe3ad9767d9c16
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-22 00:14:43 +0100

    React Router

--- React/prof/src/App.js
+++ React/prof/src/App.js
@@ -1,14 +1,22 @@
 //Global Style
 import GolbalStyle from "./components/GlobalStyle";
-import Nav from'./components/Nav';
+import Nav from "./components/Nav";
 //pages
-import AboutUs from'./pages/AboutUs';
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Router
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <GolbalStyle />
-      <Nav></Nav>
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={AboutUs}/> 
+        <Route exact path="/work" component={OurWork}/> 
+        <Route exact path="/contact" component={ContactUs}/>
+      </Switch>
     </div>
   );
 }
--- React/prof/src/components/Nav.js
+++ React/prof/src/components/Nav.js
@@ -1,49 +1,52 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Nav=()=>{
-    return (
-        <StyledNav>
-            <h1><a id="logo" href="#">Capture</a></h1>
-            <ul>
-                <li>
-                    <a href="#">1.About Us</a>
-                </li>
-                <li>
-                    <a href="#">2.Our Work</a>
-                </li>
-                <li>
-                    <a href="#">3.Contac Us</a>
-                </li>
-                
-            </ul>
-        </StyledNav>
-
-    )
-}
-const StyledNav=styled.nav`
-    min-height:10vh;
-    display:flex;
-    margin:auto;
-    justify-content:space-between;
-    align-items:center;
-    padding:1rem 10rem;
-    background:#282828;
-    a{
-        color:white;
-        text-decoration:none; 
-    }
-    ul{
-        display:flex;
-        list-style:none;
-    }
-    #logo {
-        font-size:1.8rem;
-        font-family:"Lobster",cursive;
-        font-weight:lighter;
-    }
-    li{
-        padding-left:10rem;
-        position:relative;
-    }
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">1.About Us</Link>
+        </li>
+        <li>
+          <Link to="/work">2.Our Work</Link>
+        </li>
+        <li>
+          <Link to="/contact">3.Contac Us</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.8rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
 `;
 export default Nav;
