6e42b443a006da9a73d8b6f92c1f8d75c582ac11
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-21 23:41:00 +0100

    8.Creat Nav

--- React/prof/src/App.js
+++ React/prof/src/App.js
@@ -1,11 +1,13 @@
 //Global Style
 import GolbalStyle from "./components/GlobalStyle";
+import Nav from'./components/Nav';
 //pages
 import AboutUs from'./pages/AboutUs';
 function App() {
   return (
     <div className="App">
       <GolbalStyle />
+      <Nav></Nav>
       <AboutUs />
     </div>
   );
