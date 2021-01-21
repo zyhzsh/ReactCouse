6296921660515ce81dda9c1e8bdaa66c0a4c5751
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-21 16:03:38 +0100

    4.Faq Section

--- React/prof/src/pages/AboutUs.js
+++ React/prof/src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSections";
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
