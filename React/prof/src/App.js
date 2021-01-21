c721266d9c0264b99b77527cc142c95b3bb0be05
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-21 22:18:55 +0100

    5.Intro To Styled Components

--- React/prof/src/App.js
+++ React/prof/src/App.js
@@ -1,8 +1,11 @@
+//Global Style
+import GolbalStyle from "./components/GlobalStyle";
 //pages
 import AboutUs from'./pages/AboutUs';
 function App() {
   return (
     <div className="App">
+      <GolbalStyle />
       <AboutUs />
     </div>
   );
--- React/prof/src/components/AboutSection.js
+++ React/prof/src/components/AboutSection.js
@@ -1,26 +1,64 @@
-import home1  from '../img/home1.png';
-const AboutSection=()=>{
-    return (
-        <div>
-            <div className="decription">
-                <div className="title">
-                    <div className="hide">
-                        <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>true.</h2>
-                    </div>
-                </div>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-                <button>Contact Us</button>
-            </div>
-            <div className="image">
-                <img src={home1} alt="home1"/>
-            </div>
+import home1 from "../img/home1.png";
+import styled from "styled-components";
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
         </div>
-    )
-}
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.{" "}
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="home1" />
+      </Image>
+    </About>
+  );
+};
+//Styled Components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
 export default AboutSection;
