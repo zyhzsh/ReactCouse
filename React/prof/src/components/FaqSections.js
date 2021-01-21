1abb549265a3ed4bc4e0f1408d6df6f77ffedc88
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-21 23:27:09 +0100

    7.fqa styled

--- React/prof/src/components/FaqSections.js
+++ React/prof/src/components/FaqSections.js
@@ -1,6 +1,8 @@
+import styled from "styled-components";
+import { About } from "../styles";
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -13,6 +15,7 @@ const FaqSection = () => {
             Repudiandae blanditiis corrupti sit veritatis voluptate ratione.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -23,6 +26,7 @@ const FaqSection = () => {
             Repudiandae blanditiis corrupti sit veritatis voluptate ratione.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payment Method</h4>
@@ -33,6 +37,7 @@ const FaqSection = () => {
             Repudiandae blanditiis corrupti sit veritatis voluptate ratione.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Product do you offer</h4>
@@ -43,8 +48,35 @@ const FaqSection = () => {
             Repudiandae blanditiis corrupti sit veritatis voluptate ratione.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+const Faq=styled(About)`
+  display:block;
+  span{
+    display:block;
+  }
+  h2{
+    padding-bottom:2rem;
+    font-weight:lighter;
+  }
+  .faq-line{
+    background:#ccc;
+    height:0.2rem;
+    margin:2rem 0rem;
+    width:100%;
+  }
+  .question{
+    padding:3rem 0rem;
+    cursor: pointer;
+  }
+  .answer{
+    padding:2rem 0rem;
+    p{
+      padding:1rem 0rem;
+    }
+  }
+`;
 export default FaqSection;
--- React/prof/src/components/GlobalStyle.js
+++ React/prof/src/components/GlobalStyle.js
@@ -33,6 +33,7 @@ h3{
 }
 h4{
     font-weight:bold;
+    font-size:2rem;
 }
 a{
     font-size:1.1rem;
