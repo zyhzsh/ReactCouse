293f87b1e39bcd1c97da2bcb76ab42f59f501792
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-22 14:51:41 +0100

    11.Movie Details

--- React/prof/src/App.js
+++ React/prof/src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
 //Router
 import { Switch, Route } from "react-router-dom";
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/" component={AboutUs}/> 
         <Route exact path="/work" component={OurWork}/> 
         <Route exact path="/contact" component={ContactUs}/>
+        <Route exact path="/work/:id" component={MovieDetail}/>
       </Switch>
     </div>
   );
