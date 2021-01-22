489c30a79aaab1853e3281550b275c076c5ebc8e
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-22 21:13:48 +0100

    16. Wave Animation Issues solved

--- React/prof/src/pages/OurWork.js
+++ React/prof/src/pages/OurWork.js
@@ -1,24 +1,46 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+//Images
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
-
+//Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import {
+  sliderContainer,
+  slider,
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+} from "../animation";
 
 const OurWork = () => {
   return (
-    <Work exit="exit" variants={pageAnimation} initial="hidden" animate="show">
+    <Work
+      style={{ background: "#fff" }}
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="athlete" />
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>The Reacer</h2>
+        <h2>The Racer</h2>
         <div className="line"></div>
         <Link to="/work/the-racer">
           <img src={theracer} alt="theracer" />
@@ -34,27 +56,50 @@ const OurWork = () => {
     </Work>
   );
 };
+
 const Work = styled(motion.div)`
   min-height: 100vh;
-  overflow:hidden;
+  overflow: hidden;
   padding: 5rem 10rem;
-  background:#fff;
-  h2{
-    padding:1rem 0rem;
+  h2 {
+    padding: 1rem 0rem;
   }
 `;
 const Movie = styled.div`
   padding-bottom: 10rem;
-  .line{
-    height:0.5rem;
-    background:#ccc;
-    margin-bottom:3rem;
+  .line {
+    height: 0.5rem;
+    background: #23d997;
+    margin-bottom: 3rem;
   }
-  img{
-    width:100%;
-    height:70vh;
-    object-fit:cover;
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
   }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 export default OurWork;
