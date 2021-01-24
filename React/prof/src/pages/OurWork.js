3c45e331d191c3e1b2d7c9aaab04169b59565874
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-24 15:22:25 +0100

    20 Our Work Animation

--- React/prof/src/pages/OurWork.js
+++ React/prof/src/pages/OurWork.js
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 //Images
@@ -13,9 +14,13 @@ import {
   fade,
   photoAnim,
   lineAnim,
+  swoopAdoop,
 } from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
   return (
     <Work
       style={{ background: "#fff" }}
@@ -39,16 +44,23 @@ const OurWork = () => {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+
+      <Movie ref={element} variants={fade} animate={controls} initial="hidden">
         <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-racer">
           <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
-      <Movie>
+
+      <Movie
+        ref={element2}
+        variants={fade}
+        animate={controls2}
+        initial="hidden"
+      >
         <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/good-times">
           <img src={goodtimes} alt="goodtimes" />
         </Link>
@@ -61,12 +73,14 @@ const Work = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
+
   h2 {
     padding: 1rem 0rem;
   }
 `;
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
+
   .line {
     height: 0.5rem;
     background: #23d997;
