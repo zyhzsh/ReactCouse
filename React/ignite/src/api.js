6bb77b93e206a6112acec13d983e7cd5faec3429
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-25 21:28:08 +0100

    2.Setting up Api

---
+++ React/ignite/src/api.js
@@ -0,0 +1,31 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+//Getting the date
+const getCurrentMonth = () => {
+    const month = new Date().getMonth()+1;
+    if (month < 10) {
+      return `0${month}`;
+    } else {
+      return month;
+    }
+  };
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+//Current day/month/year
+const currentYear=new Date().getFullYear();
+const currentMoth=getCurrentMonth();
+const currentDay=getCurrentDay();
+const currentDate=`${currentYear}-${currentMoth}-${currentDay}`;
+const lastYear=`${currentYear-1}-${currentMoth}-${currentDay}`;
+const nextYear=`${currentYear+1}-${currentMoth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL=()=>`${base_url}${popular_games}`;
