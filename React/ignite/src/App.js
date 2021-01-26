132c81fb5f44a67bf79053e68c08dbfeb03c778a
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-26 16:05:29 +0100

    8. Syling & 9. Global Styling

--- React/ignite/src/App.js
+++ React/ignite/src/App.js
@@ -1,11 +1,12 @@
 //Components and pages
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
 }
-
 export default App;
--- React/ignite/src/pages/Home.js
+++ React/ignite/src/pages/Home.js
@@ -1,16 +1,74 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+//Redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../acntions/gamesAction";
+//Components
+import Game from "../components/Game";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
 const Home = () => {
+  //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
-  return(
-    <div>
-        <h1>home</h1>
-    </div>
+  }, [dispatch]);
+  //Get Data Back
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+  return (
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            img={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>Popular Games</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            img={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            img={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+const GameList = styled(motion.div)`
+  padding:0rem 5rem;
+  h2{
+    padding:5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height:80vh;
+  display:grid;
+  grid-template-columns:repeat(auto-fit,minmax(500px,1fr));
+  grid-column-gap:3rem;
+  grid-row-gap:5rem;
+`;
 
 export default Home;
