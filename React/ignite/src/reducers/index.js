e5afcf2ab0b761e1d0049f5957584ee674ba8ebf
zyhzsh <67240191+zyhzsh@users.noreply.github.com>
2021-01-26 11:55:09 +0100

    4. Redux Thunk DevTools SetUp

--- React/ignite/src/reducers/index.js
+++ React/ignite/src/reducers/index.js
@@ -2,6 +2,5 @@ import { combineReducers } from "redux";
 import gamesReducer from "./gamesReducer";
 const rootReducer=combineReducers({
     games:gamesReducer,
-    user:userReducer,
 });
 export default rootReducer;
